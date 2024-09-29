38340930a880d77468290301b9ab9b3673df7df0
1dupuis <uaintscammingme2@gmail.com>
2024-09-28 22:44:06 -0400

    Create gallery-config.js

---
+++ gallery-config.js
@@ -0,0 +1,16 @@
+// gallery-config.js
+
+const galleryConfig = {
+    events: [
+        { src: 'cover.png', caption: 'Featured Image 1' },
+    ],
+    featured: [
+        { src: 'cover.png', caption: 'Featured Image 1' },
+    ],
+    portraits: [
+        { src: 'cover.png', caption: 'Featured Image 1' },
+    ],
+    sports: [
+        { src: 'cover.png', caption: 'Featured Image 1' },
+    ],
+};
