c146600e8ec8ee2f229cbb42f7a76588c9bf062d
1dupuis <uaintscammingme2@gmail.com>
2024-09-29 19:48:23 -0400

    Update gallery-config.js

--- gallery-config.js
+++ gallery-config.js
@@ -1,8 +1,19 @@
 // gallery-config.js
 
 const galleryConfig = {
-    events: [
-        { src: 'cover.png', caption: 'Featured Image 1' },
+    [lep-athletic-banquet]: [
+        { src: '_MG_4395', caption: 'Featured Image 1' },
+        { src: '_MG_4399', caption: 'Featured Image 1' },
+        { src: '_MG_4405', caption: 'Featured Image 1' },
+        { src: '_MG_4406', caption: 'Featured Image 1' },
+        { src: '_MG_4409-Enhanced-NR', caption: 'Featured Image 1' },
+        { src: '_MG_4412', caption: 'Featured Image 1' },
+        { src: '_MG_4415', caption: 'Featured Image 1' },
+        { src: '_MG_4418-Enhanced-NR', caption: 'Featured Image 1' },
+        { src: '_MG_4422', caption: 'Featured Image 1' },
+        { src: '_MG_4425', caption: 'Featured Image 1' },
+        { src: '_MG_4427', caption: 'Featured Image 1' },
+        { src: '_MG_4429', caption: 'Featured Image 1' },
     ],
     featured: [
         { src: 'cover.png', caption: 'Featured Image 1' },
